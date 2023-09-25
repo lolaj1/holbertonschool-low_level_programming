9233e42e24767fc359849cab4fafcf808c0142bc
lolaj1 <lolashanell.23@gmail.com>
2023-09-25 03:27:40 -0700

    newlanguage

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point of the program
+ *
+ * printf - print the line needed
+ *
+ * Return: upon no errors, returns a 0.
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
