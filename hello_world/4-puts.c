faf662c225d27cacce8d9bf25772c2024a94a49c
lolaj1 <lolashanell.23@gmail.com>
2023-09-25 02:42:24 -0700

    nprogramming

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* NOtes */
+
+/**
+ *
+ * main-entry point
+ *
+ * return:returns a 0 upon quest
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
